c4856b5ce742a40013192a8fc2ca63dec5007b12
KhaingKhant <44282201+KhaingKhant@users.noreply.github.com>
2020-05-17 20:43:14 +0630

    Add file via upload

---
+++ OD-Matrix/src/main/java/od/app/OdMatrixApplication.java
@@ -0,0 +1,13 @@
+package od.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdMatrixApplication.class, args);
+	}
+
+}
---
+++ OD-Matrix/src/test/java/ODGroupODMatrix/OdMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package ODGroupODMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
